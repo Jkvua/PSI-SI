1cc6da5efe12c9aa952a790e887d0597342dfb80
camila ferreira de almeida <mila.e.ferreira28@gmail.com>
2026-05-18 23:27:49 -0300

    feat: create requirements style and gitinore

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+#from view import 
+
+st.set_page_config(
+    page_title="PSI-SI", 
+    page_icon=":bar_chart:", 
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+with open("assets/style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
